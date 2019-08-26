ab1fef741620022cfef5dc113301715141aea6b9
srikanth <margamsrikanth7@gmail.com>
2019-08-26 16:55:24 +0530

    fixing mail issues#6

--- config/application.rb
+++ config/application.rb
@@ -6,6 +6,8 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+ENV.update YAML.load_file('config/application.yml')[Rails.env] rescue {}
+
 module TasksMonitoringApp
   class Application < Rails::Application
      
